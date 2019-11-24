3a90204edcdbb26d565438405f766c7223ae7488
handanunal <handan.unal@yahoo.com>
2019-11-24 18:02:49 +0300

    Conference - changing the default port application

--- src/main/java/com/pluralsight/conferencedemo/models/Session.java
+++ src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -1,26 +1,36 @@
 package com.pluralsight.conferencedemo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.List;
 
-@Entity(name = "sessions")
+@Entity(name="sessions")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Session {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long session_id;
     private String session_name;
     private String session_description;
     private Integer session_length;
+
     @ManyToMany
     @JoinTable(
-            name= "session_speakers",
+            name = "session_speakers",
             joinColumns = @JoinColumn(name = "session_id"),
             inverseJoinColumns = @JoinColumn(name = "speaker_id"))
     private List<Speaker> speakers;
 
     public Session() {
+    }
 
+    public List<Speaker> getSpeakers() {
+        return speakers;
+    }
+
+    public void setSpeakers(List<Speaker> speakers) {
+        this.speakers = speakers;
     }
 
     public Long getSession_id() {
@@ -53,13 +63,5 @@ public class Session {
 
     public void setSession_length(Integer session_length) {
         this.session_length = session_length;
-    }
-
-    public List<Speaker> getSpeakers() {
-        return speakers;
-    }
-
-    public void setSpeakers(List<Speaker> speakers) {
-        this.speakers = speakers;
     }
 }
--- src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
+++ src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
@@ -1,47 +1,34 @@
 package com.pluralsight.conferencedemo.models;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.List;
 
-import org.hibernate.annotations.Type;
-
-@Entity(name = "speakers")
+@Entity
+@Table(name="speakers")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Speaker {
-
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long speaker_id;
+
     private String first_name;
     private String last_name;
     private String title;
     private String company;
-    private String bio;
+    private String speaker_bio;
 
     @Lob
-    @Type(type="org.hibernate.type.BinaryType")
     private byte[] speaker_photo;
 
-    public Speaker() {
-
-    }
-
     @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
     private List<Session> sessions;
 
-    public List<Session> getSessions() {
-        return sessions;
-    }
-
-    public void setSessions(List<Session> sessions) {
-        this.sessions = sessions;
+    public Speaker() {
     }
 
     public byte[] getSpeaker_photo() {
@@ -50,6 +37,14 @@ public class Speaker {
 
     public void setSpeaker_photo(byte[] speaker_photo) {
         this.speaker_photo = speaker_photo;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
     }
 
     public Long getSpeaker_id() {
@@ -92,11 +87,11 @@ public class Speaker {
         this.company = company;
     }
 
-    public String getBio() {
-        return bio;
+    public String getSpeaker_bio() {
+        return speaker_bio;
     }
 
-    public void setBio(String bio) {
-        this.bio = bio;
+    public void setSpeaker_bio(String speaker_bio) {
+        this.speaker_bio = speaker_bio;
     }
 }
